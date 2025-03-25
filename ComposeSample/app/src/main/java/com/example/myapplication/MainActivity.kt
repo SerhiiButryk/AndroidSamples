4e14c084ee8272516e50b4217a671f24c28fd0ca
Serhii <sergeybutr@gmail.com>
2025-03-25 12:10:53 +0200

    update

--- ComposeSample/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ ComposeSample/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,21 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ElevatedButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.myapplication.data.SampleData
 import com.example.myapplication.ui.Conversation
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -32,39 +20,6 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Surface(modifier = Modifier.fillMaxSize()) {
                     Conversation(messages = SampleData.conversationSample)
-                }
-            }
-        }
-    }
-
-    val list = listOf("John", "David")
-
-    @Preview(showBackground = true, widthDp = 320, showSystemUi = true)
-    @Composable
-    private fun PreviewTestCompose(names: List<String> = list) {
-        MyApplicationTheme {
-            Column(modifier = Modifier.padding(vertical = 4.dp)) {
-                for (name in names) {
-                    TextItem()
-                }
-            }
-        }
-    }
-
-    @Composable
-    fun TextItem(modifier: Modifier = Modifier) {
-        Surface(
-            color = MaterialTheme.colorScheme.primary,
-            modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
-            ) {
-            Row(modifier = Modifier.padding(8.dp)) {
-                // Allows to place elements relative to each other
-                Column(modifier = Modifier.weight(1f)) {
-                    Text(text = "Hello")
-                    Text(text = "John")
-                }
-                ElevatedButton(onClick = { /*TODO*/ }) {
-                    Text(text = "Show more")
                 }
             }
         }
---
+++ ComposeSample/app/src/main/java/com/example/myapplication/ui/BasicSampleUI.kt
@@ -0,0 +1,97 @@
+package com.example.myapplication.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.myapplication.ui.theme.MyApplicationTheme
+
+@Preview(showSystemUi = true, device = "spec:parent=pixel_c,orientation=portrait")
+@Composable
+private fun SampleUI() {
+    MyApplicationTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.secondaryContainer,
+            modifier = Modifier.padding(10.dp),
+            shape = MaterialTheme.shapes.medium
+        ) {
+            Column {
+
+                CategoryUI(title = "1. Text with button") {
+                    TextWithButtonItem()
+                }
+
+                CategoryUI(title = "2. Column alignment") {
+                    ColumnItem()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryUI(title: String, content: @Composable () -> Unit) {
+    Column {
+        Text(text = title)
+        content()
+    }
+}
+
+/**
+ * Weight modifier
+ */
+@Composable
+private fun TextWithButtonItem() {
+
+    val name = "Test name"
+
+    Surface(
+        color = MaterialTheme.colorScheme.primary,
+        modifier = Modifier.padding(vertical = 4.dp, horizontal = 4.dp)
+    ) {
+
+        var expanded by remember { mutableStateOf(false) }
+
+        Row(modifier = Modifier.padding(bottom = if (expanded) 16.dp else 8.dp)) {
+            // Allows to place elements relative to each other
+            Column(modifier = Modifier.weight(1f)) {
+                Text(text = "Hello")
+                Text(text = name)
+            }
+            ElevatedButton(onClick = { expanded = !expanded }) {
+                Text(text = "Show more")
+            }
+        }
+    }
+}
+
+/**
+ * Alignment property for Column
+ */
+@Composable
+private fun ColumnItem() {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colorScheme.primary), horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Text(text = "Hello")
+    }
+}
